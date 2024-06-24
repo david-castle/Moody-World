2f13cea7958b8478cc829ac07a4ec3c8272e9f17
david.olaf.castle@gmail.com <davidcastle@Davids-MacBook-Pro.local>
2024-06-24 17:39:04 -0600

    deactivate all logging

--- app/__init__.py
+++ app/__init__.py
@@ -31,33 +31,33 @@ mail.init_app(app)
 
 from app import routes, models, errors
 
-if not app.debug:
+#if not app.debug:
     # Logging to local logs
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    logging.basicConfig(filename=f'logs/moodyworld_{datetime.now().strftime("%Y%m%d")}.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-    logger = logging.getLogger()
+#    if not os.path.exists('logs'):
+#        os.mkdir('logs')
+#    logging.basicConfig(filename=f'logs/moodyworld_{datetime.now().strftime("%Y%m%d")}.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+#    logger = logging.getLogger()
     #file_handler = TimedRotatingFileHandler(f'logs/moodyworld_{datetime.now().strftime("%Y%m%d")}.log', when='midnight', backupCount=10)
     #file_handler.setFormatter(logging.Formatter(
     #    '%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s [in %(pathname)s:%(lineno)d]'
     #))
     #file_handler.setLevel(logging.INFO)
     #app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info("MoodyWorld startup")
+#    app.logger.setLevel(logging.INFO)
+#    app.logger.info("MoodyWorld startup")
     
     # Send logging email 
-    """ if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
-            toaddr = app.config['ADMINS'], subject = 'MoodyWorld Failure Notification',
-            credentials = auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler) """
+#    """ if app.config['MAIL_SERVER']:
+#        auth = None
+#        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+#            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+#        secure = None
+#        if app.config['MAIL_USE_TLS']:
+#            secure = ()
+#        mail_handler = SMTPHandler(
+#            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+#            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+#            toaddr = app.config['ADMINS'], subject = 'MoodyWorld Failure Notification',
+#            credentials = auth, secure=secure)
+#        mail_handler.setLevel(logging.ERROR)
+#        app.logger.addHandler(mail_handler) """
